5c7720879d85104eb8935e73610442f1e1d9a7ab
Aspen Erlandsson <114827498+xzips@users.noreply.github.com>
2026-01-18 21:50:05 -0500

    Add attempt to incorporate MLX but include error TBD with cmake still

--- app/src/modules/environmental/environmental.c
+++ app/src/modules/environmental/environmental.c
@@ -8,10 +8,17 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/zbus/zbus.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/task_wdt/task_wdt.h>
+#include <zephyr/sys/util.h>
 #include <drivers/bme68x_iaq.h>
 #include <date_time.h>
 #include <zephyr/smf.h>
+
+#include "mlx90632_zephyr.h"
+#if IS_ENABLED(CONFIG_APP_LED)
+#include "led_pwm.h"
+#endif
 
 #include "message_channel.h"
 #include "modules_common.h"
@@ -34,6 +41,12 @@ BUILD_ASSERT(CONFIG_APP_ENVIRONMENTAL_WATCHDOG_TIMEOUT_SECONDS >
 			"Watchdog timeout must be greater than maximum execution time");
 
 static const struct device *const sensor_dev = DEVICE_DT_GET(DT_ALIAS(gas_sensor));
+#if DT_HAS_ALIAS(expansion_3v3_en)
+static const struct gpio_dt_spec stemma_3v3_en =
+	GPIO_DT_SPEC_GET(DT_ALIAS(expansion_3v3_en), gpios);
+#endif
+
+static bool ir_temp_ready;
 
 /* Forward declarations */
 static struct s_object s_obj;
@@ -127,6 +140,123 @@ static void task_wdt_callback(int channel_id, void *user_data)
 	SEND_FATAL_ERROR_WATCHDOG_TIMEOUT();
 }
 
+#if IS_ENABLED(CONFIG_APP_LED)
+static float clamp01(float value)
+{
+	if (value < 0.0f) {
+		return 0.0f;
+	}
+	if (value > 1.0f) {
+		return 1.0f;
+	}
+	return value;
+}
+
+static void temp_to_rgb(double temp_c, uint8_t *r, uint8_t *g, uint8_t *b)
+{
+	float rf = 0.0f;
+	float gf = 0.0f;
+	float bf = 0.0f;
+
+	if (temp_c <= -10.0) {
+		rf = 0.0f;
+		gf = 0.0f;
+		bf = 1.0f;
+	} else if (temp_c >= 60.0) {
+		rf = 1.0f;
+		gf = 0.0f;
+		bf = 0.0f;
+	} else if (temp_c < 25.0) {
+		float t = (float)((temp_c + 10.0) / 35.0);
+		rf = 0.0f;
+		gf = t;
+		bf = 1.0f - t;
+	} else {
+		float t = (float)((temp_c - 25.0) / 35.0);
+		rf = t;
+		gf = 1.0f - t;
+		bf = 0.0f;
+	}
+
+	*r = (uint8_t)(clamp01(rf) * 255.0f);
+	*g = (uint8_t)(clamp01(gf) * 255.0f);
+	*b = (uint8_t)(clamp01(bf) * 255.0f);
+}
+
+static void update_temp_led(double temp_c)
+{
+	uint8_t r = 0;
+	uint8_t g = 0;
+	uint8_t b = 0;
+	int ret;
+
+	temp_to_rgb(temp_c, &r, &g, &b);
+	ret = led_pwm_set_rgb(r, g, b);
+	if (ret) {
+		LOG_WRN("Failed to set temp LED color: %d", ret);
+	}
+}
+#else
+static void update_temp_led(double temp_c)
+{
+	ARG_UNUSED(temp_c);
+}
+#endif
+
+static int ir_temp_init(void)
+{
+	int ret;
+
+#if DT_HAS_ALIAS(expansion_3v3_en)
+	/* IR temp init: power the external Stemma connector before I2C access. */
+	if (!device_is_ready(stemma_3v3_en.port)) {
+		LOG_ERR("EXPANSION_3V3_EN GPIO not ready");
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&stemma_3v3_en, GPIO_OUTPUT_ACTIVE);
+	if (ret) {
+		LOG_ERR("Failed to enable EXPANSION_3V3_EN: %d", ret);
+		return ret;
+	}
+
+	k_msleep(500);
+#endif
+
+	/* IR temp init: initialize the MLX90632 after power is on. */
+	ret = (int)mlx90632_zephyr_init();
+	if (ret < 0) {
+		return ret;
+	}
+
+	ir_temp_ready = true;
+	return 0;
+}
+
+static int ir_temp_sample(double *object_c)
+{
+	double ambient_c = 0.0;
+	double object_c_local = 0.0;
+	int ret;
+
+	if (object_c == NULL) {
+		return -EINVAL;
+	}
+
+	if (!ir_temp_ready) {
+		return -EACCES;
+	}
+
+	/* IR temp measurement: read MLX90632 object temperature. */
+	ret = (int)mlx90632_zephyr_sample(&ambient_c, &object_c_local);
+	if (ret < 0) {
+		return ret;
+	}
+
+	*object_c = object_c_local;
+	return 0;
+}
+
 static void sample(void)
 {
 	int64_t system_time;
@@ -137,7 +267,11 @@ static void sample(void)
 	struct sensor_value iaq = { 0 };
 	struct sensor_value co2 = { 0 };
 	struct sensor_value voc = { 0 };
+	struct sensor_value temp_reported = { 0 };
 	struct env_object env_obj = { 0 };
+	double ir_object_c = 0.0;
+	double temp_reported_c;
+	bool ir_ok = false;
 	int ret;
 
 	ret = sensor_sample_fetch(sensor_dev);
@@ -155,9 +289,32 @@ static void sample(void)
 	ret = sensor_channel_get(sensor_dev, SENSOR_CHAN_VOC, &voc);
 	__ASSERT_NO_MSG(ret == 0);
 
-	LOG_DBG("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d; iaq: %d; CO2: %d.%06d; "
+	temp_reported = temp;
+	temp_reported_c = sensor_value_to_double(&temp);
+
+	/* IR temp measurement: prefer MLX90632 object temperature over BME ambient. */
+	if (ir_temp_ready) {
+		ret = ir_temp_sample(&ir_object_c);
+		if (ret == 0) {
+			ir_ok = true;
+			ret = sensor_value_from_double(&temp_reported, ir_object_c);
+			if (ret == 0) {
+				temp_reported_c = ir_object_c;
+			} else {
+				LOG_WRN("Failed to convert IR temperature: %d", ret);
+				ir_ok = false;
+				temp_reported = temp;
+				temp_reported_c = sensor_value_to_double(&temp);
+			}
+		} else {
+			LOG_WRN("MLX90632 sample failed: %d", ret);
+		}
+	}
+
+	LOG_DBG("temp(%s): %d.%06d; press: %d.%06d; humidity: %d.%06d; iaq: %d; CO2: %d.%06d; "
 		"VOC: %d.%06d",
-		temp.val1, temp.val2, press.val1, press.val2, humidity.val1, humidity.val2,
+		ir_ok ? "ir" : "bme",
+		temp_reported.val1, temp_reported.val2, press.val1, press.val2, humidity.val1, humidity.val2,
 		iaq.val1, co2.val1, co2.val2, voc.val1, voc.val2);
 
 	ret = date_time_now(&system_time);
@@ -167,10 +324,14 @@ static void sample(void)
 	}
 
 	env_obj.temperature_m.bt = (int32_t)(system_time / 1000);
-	env_obj.temperature_m.vf = sensor_value_to_double(&temp);
+	/* Substitute IR temperature for cloud reporting (fallback to BME if needed). */
+	env_obj.temperature_m.vf = temp_reported_c;
 	env_obj.humidity_m.vf = sensor_value_to_double(&humidity);
 	env_obj.pressure_m.vf = sensor_value_to_double(&press) / 100;
 	env_obj.iaq_m.vi = iaq.val1;
+
+	/* Update LED color based on the reported temperature. */
+	update_temp_led(temp_reported_c);
 
 	ret = cbor_encode_env_object(payload.buffer, sizeof(payload.buffer),
 				     &env_obj, &payload.buffer_len);
@@ -201,6 +362,12 @@ static void environmental_task(void)
 	LOG_DBG("Environmental module task started");
 
 	task_wdt_id = task_wdt_add(wdt_timeout_ms, task_wdt_callback, (void *)k_current_get());
+
+	/* IR temp init: power/initialize the sensor once before sampling. */
+	err = ir_temp_init();
+	if (err) {
+		LOG_WRN("MLX90632 init failed: %d", err);
+	}
 
 	STATE_SET_INITIAL(STATE_INIT);
 
